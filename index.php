8ea81dd11739e8f03ab2612e05687e0ddb46e2f1
Paralizers <darkparalizer@gmail.com>
2018-10-08 21:47:39 +0200

    a

--- index.php
+++ index.php
@@ -29,6 +29,7 @@
 		file_put_contents(json_encode($userDecode),$UserFile);
 	}
 	$content = file_get_contents("php://input");
+	file_put_contents("input.json",$content);
 	$update = json_decode($content, true);
 	$token = "553791725:AAEg_xmne9OYFNjqiak7ORJoW7mm4pqPcLo";
 	if($update){
