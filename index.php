dd55daede95009973beca044311e6fbd8aec7353
Paralizers <darkparalizer@gmail.com>
2018-10-08 21:30:07 +0200

    a

--- index.php
+++ index.php
@@ -1,11 +1,21 @@
 <?php
 	error_reporting(E_ALL);
-	function sendMessageBot($chatId,$message){
+	function sendMessageBot($chatId,$userId,$message){
+		global $token;
+		$UserFile = "users.json"; //File utente
+		$userJson = file_get_contents();
+		// Leggo File Utente
+		$userDecode = json_decode($userJson);
+		if($userDecode[$userId] && $userDecode[$userId] > time()){
+			return false;
+		}
+		$userDecode[$userId] = strtotime("+2 minutes");
+		//Setto variabile Utente
 		$data = array(
         'chat_id' => urlencode($chatId),
         'text' => urlencode($message)
 		);
-		print_r($data);
+		
 		$url ="https://api.telegram.org/bot{$token}/sendMessage";
 		$ch = curl_init();
 		curl_setopt($ch, CURLOPT_URL, $url);
@@ -13,17 +23,19 @@
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		$result = curl_exec($ch);
-		file_put_contents("result.json",$result);
 		curl_close($ch);
+		
+		//Invia File
+		file_put_contents(json_encode($userDecode),$UserFile);
 	}
-	$content = file_get_contents("test2.json");
+	$content = file_get_contents("php://input");
 	$update = json_decode($content, true);
 	$token = "553791725:AAEg_xmne9OYFNjqiak7ORJoW7mm4pqPcLo";
 	print_r($update);
 	if($update){
 		$message = isset($update['message']) ? $update['message'] : "";
+		$userId = isset($update["from"]["id"]) : $update["from"]["id"] : "";
 		$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-		print_r($update);
-		sendMessageBot($chatId,"Prova123");
+		if($userId && $chatId)sendMessageBot($chatId,$userId,"Prova123");
 	}
 ?>
